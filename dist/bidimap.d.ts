7e86cfa0b0ed88019e76063576c754d082cd9111
hiroto7 <hiroto450@gmail.com>
2018-10-11 02:59:34 +0900

    [Add] bidimap.tsのドキュメントを加筆

--- dist/bidimap.d.ts
+++ dist/bidimap.d.ts
@@ -1,17 +1,61 @@
+/**
+ * @param K キーの型
+ * @param V 値の型
+ */
 export interface ReadonlyBidiMap<K, V> extends ReadonlyMap<K, V> {
+    /**
+     * 逆方向のマップ、すなわち値からキーへのマップです。
+     */
     readonly inverse: ReadonlyBidiMap<V, K>;
+    /**
+     * キー・値の両方から重複を取り除いたマップを返します。
+     */
     dedupe(): ReadonlyBidiMap<K, V>;
+    /**
+     * マップのすべてのキー・値に対して指定された関数を実行します。
+     * @param callbackfn それぞれのキー・値に対して実行する関数
+     * @param thisArg `callbackfn`を実行するとき、`this`として使うオブジェクト
+     */
     forEach(callbackfn: (value: V, key: K, map: ReadonlyBidiMap<K, V>) => void, thisArg?: any): void;
 }
 export declare namespace ReadonlyBidiMap {
+    /**
+     * オブジェクトが`ReadonlyBidiMap`または`BidiMap`であるかどうかを判定します。
+     * @param K マップのキーの型
+     * @param V マップの値の型
+     * @param iterable 判定するオブジェクト
+     * @returns オブジェクトが`ReadonlyBidiMap`または`BidiMap`であれば`true`、そうでなければ`false`
+     */
     function isReadonlyBidiMap<K, V>(iterable: Iterable<[K, V]> | any): iterable is ReadonlyBidiMap<K, V>;
 }
+/**
+ * @param K キーの型
+ * @param V 値の型
+ */
 export interface BidiMap<K, V> extends ReadonlyBidiMap<K, V>, Map<K, V> {
+    /**
+     * 逆方向のマップ、すなわち値からキーへのマップです。
+     */
     readonly inverse: BidiMap<V, K>;
+    /**
+     * キー・値の両方から重複を取り除いたマップを返します。
+     */
     dedupe(): BidiMap<K, V>;
+    /**
+     * マップのすべてのキー・値に対して指定された関数を実行します。
+     * @param callbackfn それぞれのキー・値に対して実行する関数
+     * @param thisArg `callbackfn`を実行するとき、`this`として使うオブジェクト
+     */
     forEach(callbackfn: (value: V, key: K, map: BidiMap<K, V>) => void, thisArg?: any): void;
 }
 export declare namespace BidiMap {
+    /**
+     * オブジェクトが`BidiMap`であるかどうかを判定します。
+     * @param K マップのキーの型
+     * @param V マップの値の型
+     * @param iterable 判定するオブジェクト
+     * @returns オブジェクトが`BidiMap`であれば`true`
+     */
     function isBidiMap<K, V>(iterable: Iterable<[K, V]> | any): iterable is BidiMap<K, V>;
 }
 declare abstract class AbstractBidiMap<K, V> implements BidiMap<K, V> {
@@ -21,11 +65,41 @@ declare abstract class AbstractBidiMap<K, V> implements BidiMap<K, V> {
     readonly [Symbol.toStringTag]: 'Map';
     readonly size: number;
     dedupe(): BidiMap<K, V>;
+    /**
+     * このマップおよび`this.inverse`からすべてのマッピングを削除します。
+     */
     clear(): void;
+    /**
+     * 指定されたキーのマッピングが存在する場合、これを削除します。
+     * 逆方向のマッピングが`this.inverse`に存在すれば、これも削除します。
+     * @param key
+     */
     delete(key: K): boolean;
+    /**
+     * マップのすべてのキー・値に対して指定された関数を実行します。
+     * @param callbackfn それぞれのキー・値に対して実行する関数
+     * @param thisArg `callbackfn`を実行するとき、`this`として使うオブジェクト
+     */
     forEach(callbackfn: (value: V, key: K, map: BidiMap<K, V>) => void, thisArg?: any): void;
+    /**
+     * 指定されたキーがマッピングしている値を返します。
+     * マッピングが存在しなければ`undefined`を返します。
+     * @param key マッピングのキー
+     * @returns マッピングの値、存在しなければ`undefined`
+     */
     get(key: K): V | undefined;
+    /**
+     * 指定されたキーのマッピングが存在するかどうかを判定します。
+     * @param key マッピングのキー
+     * @returns マッピングが存在すれば`true`、存在しなければ`false`
+     */
     has(key: K): boolean;
+    /**
+     * 指定されたキーから値への、新しいマッピングを追加します。
+     * 同時に`this.inverse`には、値からキーへのマッピングが追加されます。
+     * @param key マッピングのキー
+     * @param value マッピングの値
+     */
     set(key: K, value: V): this;
     [Symbol.iterator](): IterableIterator<[K, V]>;
     entries(): IterableIterator<[K, V]>;
@@ -33,6 +107,10 @@ declare abstract class AbstractBidiMap<K, V> implements BidiMap<K, V> {
     values(): IterableIterator<V>;
     toJSON(): any;
 }
+/**
+ * @param K キーの型
+ * @param V 値の型
+ */
 export declare class DualBidiMap<K, V> extends AbstractBidiMap<K, V> {
     protected readonly xToY: Map<K, V>;
     protected readonly yToX: Map<V, K>;
--- src/bidimap.ts
+++ src/bidimap.ts
@@ -3,22 +3,62 @@
  * @param V 値の型
  */
 export interface ReadonlyBidiMap<K, V> extends ReadonlyMap<K, V> {
+  /**
+   * 逆方向のマップ、すなわち値からキーへのマップです。
+   */
   readonly inverse: ReadonlyBidiMap<V, K>;
+  /**
+   * キー・値の両方から重複を取り除いたマップを返します。
+   */
   dedupe(): ReadonlyBidiMap<K, V>;
+  /**
+   * マップのすべてのキー・値に対して指定された関数を実行します。
+   * @param callbackfn それぞれのキー・値に対して実行する関数
+   * @param thisArg `callbackfn`を実行するとき、`this`として使うオブジェクト
+   */
   forEach(callbackfn: (value: V, key: K, map: ReadonlyBidiMap<K, V>) => void, thisArg?: any): void;
 }
 export namespace ReadonlyBidiMap {
+  /**
+   * オブジェクトが`ReadonlyBidiMap`または`BidiMap`であるかどうかを判定します。
+   * @param K マップのキーの型
+   * @param V マップの値の型
+   * @param iterable 判定するオブジェクト
+   * @returns オブジェクトが`ReadonlyBidiMap`または`BidiMap`であれば`true`、そうでなければ`false`
+   */
   export function isReadonlyBidiMap<K, V>(iterable: Iterable<[K, V]> | any): iterable is ReadonlyBidiMap<K, V> {
     return iterable instanceof AbstractBidiMap;
   }
 }
 
+/**
+ * @param K キーの型
+ * @param V 値の型
+ */
 export interface BidiMap<K, V> extends ReadonlyBidiMap<K, V>, Map<K, V> {
+  /**
+   * 逆方向のマップ、すなわち値からキーへのマップです。
+   */
   readonly inverse: BidiMap<V, K>;
+  /**
+   * キー・値の両方から重複を取り除いたマップを返します。
+   */
   dedupe(): BidiMap<K, V>;
+  /**
+   * マップのすべてのキー・値に対して指定された関数を実行します。
+   * @param callbackfn それぞれのキー・値に対して実行する関数
+   * @param thisArg `callbackfn`を実行するとき、`this`として使うオブジェクト
+   */
   forEach(callbackfn: (value: V, key: K, map: BidiMap<K, V>) => void, thisArg?: any): void;
 }
 export namespace BidiMap {
+  /**
+   * オブジェクトが`BidiMap`であるかどうかを判定します。
+   * @param K マップのキーの型
+   * @param V マップの値の型
+   * @param iterable 判定するオブジェクト
+   * @returns オブジェクトが`BidiMap`であれば`true`
+   */
   export function isBidiMap<K, V>(iterable: Iterable<[K, V]> | any): iterable is BidiMap<K, V> {
     return iterable instanceof AbstractBidiMap;
   }
@@ -40,11 +80,19 @@ abstract class AbstractBidiMap<K, V> implements BidiMap<K, V> {
     return new DualBidiMap<K, V>(inverseLike.inverse);
   }
 
+  /**
+   * このマップおよび`this.inverse`からすべてのマッピングを削除します。
+   */
   clear(): void {
     this.xToY.clear();
     this.yToX.clear();
   }
 
+  /**
+   * 指定されたキーのマッピングが存在する場合、これを削除します。
+   * 逆方向のマッピングが`this.inverse`に存在すれば、これも削除します。
+   * @param key 
+   */
   delete(key: K): boolean {
     if (this.xToY.has(key)) {
       const value: V = this.xToY.get(key)!;
@@ -55,18 +103,40 @@ abstract class AbstractBidiMap<K, V> implements BidiMap<K, V> {
     return this.xToY.delete(key);
   }
 
+  /**
+   * マップのすべてのキー・値に対して指定された関数を実行します。
+   * @param callbackfn それぞれのキー・値に対して実行する関数
+   * @param thisArg `callbackfn`を実行するとき、`this`として使うオブジェクト
+   */
   forEach(callbackfn: (value: V, key: K, map: BidiMap<K, V>) => void, thisArg?: any): void {
     this.forEach(callbackfn, thisArg);
   }
 
+  /**
+   * 指定されたキーがマッピングしている値を返します。
+   * マッピングが存在しなければ`undefined`を返します。
+   * @param key マッピングのキー
+   * @returns マッピングの値、存在しなければ`undefined`
+   */
   get(key: K): V | undefined {
     return this.xToY.get(key);
   }
 
+  /**
+   * 指定されたキーのマッピングが存在するかどうかを判定します。
+   * @param key マッピングのキー
+   * @returns マッピングが存在すれば`true`、存在しなければ`false`
+   */
   has(key: K): boolean {
     return this.xToY.has(key);
   }
 
+  /**
+   * 指定されたキーから値への、新しいマッピングを追加します。
+   * 同時に`this.inverse`には、値からキーへのマッピングが追加されます。
+   * @param key マッピングのキー
+   * @param value マッピングの値
+   */
   set(key: K, value: V): this {
     this.xToY.set(key, value);
     this.yToX.set(value, key);
@@ -94,6 +164,10 @@ abstract class AbstractBidiMap<K, V> implements BidiMap<K, V> {
   }
 }
 
+/**
+ * @param K キーの型
+ * @param V 値の型
+ */
 export class DualBidiMap<K, V> extends AbstractBidiMap<K, V> {
   protected readonly xToY: Map<K, V>;
   protected readonly yToX: Map<V, K>;
